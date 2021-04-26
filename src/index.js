e69e12dcc7d619409bbf134dab594cf506441343
FarsheedMadihiBidgoli <farsheed.madihi-bidgoli-extern@deutschebahn.com>
2021-04-26 20:48:38 +0200

    the webpack config for the production added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+} else {
+  console.log('we are in production mode!');
+}
+
+
 const title = 'frontend-service';
 
 ReactDOM.render(
